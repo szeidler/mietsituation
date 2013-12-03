bcb29da8d4408d70b1e41b710adeadbb9f9550ce
johannesoliverengl <johannesoliver.engl@googlemail.com>
2013-12-03 15:29:16 +0100

    heatmap implemented

--- script.js
+++ script.js
@@ -15,24 +15,32 @@ function initialize() {
 function fillArray(){
   var dataArray = [
     {"lng" : 52.50849,
-    "lat" : 13.403637},
-    {"lng" : 52.50849,
-    "lat" : 13.403637},
-    {"lng" : 52.50849,
-    "lat" : 13.403637},
-    {"lng" : 52.50849,
-    "lat" : 13.403637}
+    "lat" : 13.403637,
+    "mag" : 1
+    },
+    {"lng" : 52.591369,
+    "lat" : 13.434691,
+    "mag" : 2.2
+    }
   ];
   return dataArray;
 }
 
 function loadPoints(dataArray){
+  var heatmapData = [];
   for (var i = dataArray.length - 1; i >= 0; i--) {
     var latLng = new google.maps.LatLng(dataArray[i].lng, dataArray[i].lat);
+    var magnitude = dataArray[i].mag;
+    var weightedLoc = {
+      location: latLng,
+      weight: Math.pow(2, magnitude)
+    };
+    heatmapData.push(weightedLoc);
     console.log(latLng);
-    var marker = new google.maps.Marker({
-      position: latLng,
-      map: map,
+  }
+  var heatmap = new google.maps.visualization.HeatmapLayer({
+    data: heatmapData,
+    dissipating: false,
+    map: map
     });
-  };
 }
